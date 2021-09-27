a6c2dd45e83bb18b91ef6ed3efdeebb978f98eda
0xbono <iam@sonjim.in>
2021-09-28 03:14:35 +0900

    feat(App): 기본적인 캔버스 및 샘플 모델 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas, useLoader } from '@react-three/fiber'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
 function App() {
+  const carModel = useLoader(FBXLoader, '/model/Low-Poly-Racing-Car.fbx');
+  const hongikModel = useLoader(FBXLoader, '/model/hongik-sample-model.fbx');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      <ambientLight intensity={0.5} />
+      <spotLight intensity={0.6} position={[20, 90, 0]} angle={0.1} penumbra={1} shadow-mapSize-width={2048} shadow-mapSize-height={2048} castShadow />
+      <OrbitControls />
+      <mesh scale={0.01}>
+        <primitive object={carModel} dispose={null} />
+      </mesh>
+      <mesh scale={0.01}>
+        <primitive object={hongikModel} dispose={null} />
+      </mesh>
+    </Canvas>
   );
 }
 
