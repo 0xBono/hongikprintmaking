9a9a7582ed490dece16330b878f4cd9dfd3e38c1
0xbono <iam@sonjim.in>
2021-11-07 15:14:33 +0900

    feat: Work Page에서 기존 레퍼런스 페이지와 같도록 개발(모바일 제외)

--- src/components/Card/WorkItemCard/WorkItemCard.tsx
+++ src/components/Card/WorkItemCard/WorkItemCard.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './WorkItemCard.module.scss';
 
 const cx = classNames.bind(styles);
 
 export const WorkItemCard = () => {
+  const [color, setColor] = useState<string>();
+  const [mouseHover, setMouseHover] = useState<boolean>(false);
+
+  useEffect(() => {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    setColor(randomColor);
+  }, []);
   return (
-    <article className={cx('wrap')}>
+    <article
+      className={cx('wrap')}
+      onMouseEnter={() => setMouseHover(true)}
+      onMouseLeave={() => setMouseHover(false)}
+      style={{ backgroundColor: mouseHover ? color : '#fff' }}
+    >
       <img
         src="/assets/button/White_button.svg"
         alt="Picture of the author"
